43e00e9038d59a100ac24aca2cf3f0ee6e867802
Igor Broychenko <igorbroychenko@Igors-MacBook-Pro.local>
2026-01-21 22:50:09 +0100

    remove function

--- gpt.py
+++ gpt.py
@@ -37,9 +37,3 @@ class ChatGptService:
         self.message_list.append({"role": "system", "content": prompt_text})
         self.message_list.append({"role": "user", "content": message_text})
         return await self.send_message_list()
-
-    async def send_answer(self, prompt_text: str, message_text: str) -> str:
-        self.message_list.clear()
-
-    async def close(self) -> None:
-        self.message_list.clear()
