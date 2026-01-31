6b9d01ce1790d35f461a8a0c3f01ad8235edc140
Igor Broychenko <igorbroychenko@Igors-MacBook-Pro.local>
2026-01-31 22:24:18 +0100

    add clear message list

--- gpt.py
+++ gpt.py
@@ -43,3 +43,6 @@ class ChatGptService:
         self.message_list.append({"role": "system", "content": prompt_text})
         self.message_list.append({"role": "user", "content": message_text})
         return await self.send_message_list()
+
+    async def close(self) -> None:
+        self.message_list.clear()
