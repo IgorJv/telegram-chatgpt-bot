64c60d91a9ea309acf091394caa411b96a8b5fa8
Igor Broychenko <igorbroychenko@Igors-MacBook-Pro.local>
2026-03-02 18:33:25 +0100

    add new answer

--- gpt.py
+++ gpt.py
@@ -49,3 +49,6 @@ class ChatGptService:
         self.message_list.append({"role": "user", "content": question_text})
         self.message_list.append({"role": "system", "content": question_text})
         return await self.send_message_list()
+
+    async def add_answer(self, answer_text: str) -> str:
+        self.message_list.clear()
