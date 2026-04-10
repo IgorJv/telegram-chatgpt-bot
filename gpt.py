b629e8712f46a3f4047a34717b5800a83a636d8c
Igor Broychenko <igorbroychenko@Igors-MacBook-Pro.local>
2026-04-10 10:59:01 +0200

    remove duplicate

--- gpt.py
+++ gpt.py
@@ -56,8 +56,6 @@ class ChatGptService:
         self.message_list.append({"role": "system", "content": answer_text})
         self.message_list.append({"role": "user_local", "content": answer_text})
         self.message_list.append({"role": "system_local", "content": answer_text})
-        self.message_list.append({"role": "user", "downloads": answer_text})
-        self.message_list.append({"role": "system", "downloads": answer_text})
         self.message_list.append({"role": "user", "uploads": answer_text})
         self.message_list.append({"role": "system", "uploads": answer_text})
         return await self.send_message_list()
